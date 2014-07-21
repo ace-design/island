b96cc20cea27b1c86eaaf63f6dda9512a888e59e
Sébastien Mosser <sebastien.mosser@gmail.com>
2014-07-21 10:33:55 +0200

    Introducing map.scala

---
+++ src/main/scala/eu/ace_design/island/map/Map.scala
@@ -0,0 +1,11 @@
+package eu.ace_design.island.map
+
+import eu.ace_design.island.geom.Mesh
+
+/**
+ * A map wraps a geometrical mesh and add properties (i.e., semantics) to each faces
+ * @param mesh the geomtrical mesh used
+ */
+class Map(val mesh: Mesh) {
+
+}
