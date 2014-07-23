c2afe16dd8bff1028b3fb5d692c5151f76948137
Sébastien Mosser <sebastien.mosser@gmail.com>
2014-07-23 11:03:34 +0200

    doc + sealed trait for properties

--- src/main/scala/eu/ace_design/island/map/Properties.scala
+++ src/main/scala/eu/ace_design/island/map/Properties.scala
@@ -1,16 +1,44 @@
 package eu.ace_design.island.map
 
-trait Property[T] {
+/**
+ * A property bound a value to an immutable key.  The trait is sealed, and cannot be implemented outside of this file.
+ * @tparam T the type of value
+ */
+sealed trait Property[T] {
+  // The key associated to this property
   val key: String
+  // the value (a T)
   val value: T
 }
 
+/**
+ * A PropertySet associate to a given index a set of properties
+ * @param _contents the internal storage
+ */
 case class PropertySet(_contents: Map[Int, Set[Property[_]]]= Map()) {
 
+  /**
+   * The size of this property set (number of indexes)
+   * @return the size of the internal storage
+   */
   def size: Int = _contents.size
 
+  /**
+   * Extract the properties associated to a given index
+   * @param idx the index to look for
+   * @return the set of properties associated to idx, a NoSuchElementException if idx does not exists
+   */
   def get(idx: Int): Set[Property[_]] = _contents(idx)
 
+  /**
+   * Add a property to a given index, in a functional way. the semantics of the add of the add function is the
+   * following:
+   *   - there is no property for this index: initialise the index, and store the property
+   *   - there is existing properties, different than the one to add: add it to the set of properties
+   *   - a property value exists for this index: replace it by the new one.
+   * @param pair a couple (index -> Property) to be added in the property set
+   * @return a new property set, according to the add semantics.
+   */
   def +(pair: (Int,Property[_])): PropertySet = {
     val idx = pair._1
     val prop = pair._2
@@ -29,6 +57,12 @@ case class PropertySet(_contents: Map[Int, Set[Property[_]]]= Map()) {
     this.copy(_contents = newContents)
   }
 
+  /**
+   * Check if a property is associated to a given index
+   * @param idx the index to check
+   * @param p the property one is looking for
+   * @return a boolean indicating if the index is associated to  the property.
+   */
   def check(idx: Int, p: Property[_]): Boolean = _contents.get(idx) match {
     case None => false
     case Some(existing) => existing contains p
@@ -36,6 +70,9 @@ case class PropertySet(_contents: Map[Int, Set[Property[_]]]= Map()) {
 
 }
 
+/*********************************************
+ ** Properties available in the Island game **
+ *********************************************/
 
 case class IsWater(override val value: Boolean = true) extends Property[Boolean] {
   override val key = "isWater?"
