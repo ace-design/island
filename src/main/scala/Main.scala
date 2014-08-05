1562131a6e21d6960f8791a07dc5ae3023e7fec1
Sébastien Mosser <sebastien.mosser@gmail.com>
2014-08-05 15:45:07 +0200

    totally naive implementation of an internal DSL to configure the island. do not use

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,67 +1,16 @@
-import eu.ace_design.island.geom._
 import eu.ace_design.island.map._
-import eu.ace_design.island.viewer._
 import eu.ace_design.island.util.{LogSilos, Logger}
 
 
 
 
 
-object Main extends App with Logger {
+object Main extends App with Logger with eu.ace_design.island.dsl.DiSLand {
 
   val silo = LogSilos.ROOT
 
-  final val OUTPUT_FILE = "./map"
-  final val size = Sizes.SMALL
-
-  final val MAP_SIZE = size._1
-  final val NB_FACES = size._2
-
-  info("Starting the map generation process")
-
-  // Randomly generate the sites used to generate the map
-  //val generator = new SquaredGrid(MAP_SIZE)
-  //val generator = new RandomGrid(MAP_SIZE)
-  val generator = new RelaxedRandomGrid(MAP_SIZE)
-  val sites = generator(NB_FACES)
-
-  // Instantiate a mesh builder, and process the random sites to create a mesh
-  val meshBuilder = new MeshBuilder(MAP_SIZE)
-  val mesh = meshBuilder(sites)
-
-  // Instantiate an Island Builder, and build the map on the previously created mesh
-  val mapBuilder = new IslandBuilder {
-    import processes._
-    //final val ISLAND_SHAPE = DiskShape(size, size.toDouble/2 * 0.85)
-    //final val ISLAND_SHAPE = DonutShape(size, size.toDouble/2 * 0.85, size.toDouble/2 * 0.20)
-    final val ISLAND_SHAPE = RadialShape(size, 1.87)
-    final val WATER_THRESHOLD = 30
-    override def size: Int = MAP_SIZE
-    override protected val steps: Seq[Process] = Seq(
-      IdentifyBorders, IdentifyWaterArea(ISLAND_SHAPE, WATER_THRESHOLD),
-      IdentifyLakesAndOcean, IdentifyCoastLine
-    )
-  }
-  val map = mapBuilder(mesh)
-
-  info("End of the map generation process")
-
-  info("Starting the transformation into PDF")
-  val transformer = new PDFViewer()
-  val result = transformer(map)
-  result.renameTo(new java.io.File(OUTPUT_FILE+".pdf"))
-
-  val toOBJ = new OBJViewer()
-  val objfile = toOBJ(map)
-  objfile.renameTo(new java.io.File(OUTPUT_FILE+".obj"))
-
-  info("PDF file generated!")
-}
-
-object Sizes {
-
-  val SMALL = (100, 100)
-  val MEDIUM = (625, 625)
-  val LARGE = (2048, 4096)
+  // Creating the map
+  val medium: IslandMap = createIsland withSize 800 having 600.faces storedIn "./map-dsl" as pdf
 
 }
+
--- src/main/scala/eu/ace_design/island/dsl/DiSLand.scala
+++ src/main/scala/eu/ace_design/island/dsl/DiSLand.scala
@@ -1,7 +1,9 @@
 package eu.ace_design.island.dsl
 
-import eu.ace_design.island.geom.PointGenerator
+import eu.ace_design.island.geom._
 import eu.ace_design.island.map._
+import eu.ace_design.island.map.processes._
+import eu.ace_design.island.viewer._
 
 /**
  * This file is part of the island project
@@ -9,11 +11,68 @@ import eu.ace_design.island.map._
  **/
 trait DiSLand {
 
+  // TODO fix issue in size propagation
+  private object defaults {
+    final val SIZE: Int  = 800
+    final val FACES: Int = 600
+    final val GENERATOR: PointGenerator = new RelaxedRandomGrid(SIZE)
+    final val SHAPE: IslandShape        =  RadialShape(SIZE, 1.87)
+    final val WATER_THRESHOLD: Int      = 30
+    final val PROCESS = Seq(IdentifyBorders, IdentifyWaterArea(SHAPE, WATER_THRESHOLD),
+                            IdentifyLakesAndOcean, IdentifyCoastLine)
+    final val OUTPUT_FILE = "./map"
+    final val VIEWER      = new PDFViewer()
+  }
+
+  def createIsland = Config()
 
 
+  protected case class Config (size: Int               = defaults.SIZE,
+                             faces: Int                = defaults.FACES,
+                             generator: PointGenerator = defaults.GENERATOR,
+                             shape: IslandShape        = defaults.SHAPE,
+                             waterThreshold: Int       = defaults.WATER_THRESHOLD,
+                             process: Seq[Process]     = defaults.PROCESS,
+                             output: String            = defaults.OUTPUT_FILE,
+                             format: Viewer            = defaults.VIEWER)
+  {
+    //require(generator.size == size, "Size specification must match in all parameters")
+    //require(shape.size == size,     "Size specification must match in all parameters")
 
-  private case class Config (size: Option[Int], generator: Option[PointGenerator], shape: Option[IslandShape],
-                     waterThreshold: Option[Int], process: Seq[Process])
+    def withSize(s: Int) = this.copy(size = s)
+    def having(fi: FaceInteger) = this.copy(faces = fi.i)
+    def shapedAs(s: IslandShape) = this.copy(shape = s)
+    def storedIn(f: String) = this.copy(output = f)
+    def as(v: Viewer) = this.copy(format = v)
+
+
+    def toMap: IslandMap = {
+      val sites = generator(faces)
+      val meshBuilder = new MeshBuilder(size)
+      val mesh = meshBuilder(sites)
+      val s = size
+      val mapBuilder = new IslandBuilder {
+        override def size: Int = s
+        override protected val steps: Seq[Process] = process
+      }
+      val map = mapBuilder(mesh)
+      val out = format(map)
+      out.renameTo(new java.io.File(output+"."+format.extension))
+      map
+    }
+
+  }
+
+
+  implicit protected def integerToFaceInteger(i: Int) = new FaceInteger(i)
+  protected class FaceInteger(val i: Int) { def faces = i }
+
+  protected val pdf = new PDFViewer()
+  protected val svg = new SVGViewer()
+  protected val obj = new OBJViewer()
+
+
+  implicit protected def config2map(c: Config): IslandMap = c.toMap
 
 
 
