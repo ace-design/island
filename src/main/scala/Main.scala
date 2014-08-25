7019040d81194150659d13a3c59b6c73a6c74e80
Sebastien Mosser <sebastien.mosser@gmail.com>
2014-08-25 19:17:20 +0200

    Process can be specified as random one, and properly fed with a shared random generator when building a map

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,16 +10,20 @@ object Main extends App with Logger with DiSLand {
 
   val silo = LogSilos.ROOT
 
+  info("Configuring small island")
   val small = createIsland shapedAs donut(80.percent, 20.percent) withSize 400 having 1200.faces
   //export(small)
 
-  val medium = createIsland shapedAs radial(factor = 1.37) withSize 800 having 1024.faces
+  info("Configuring medium island")
+  val medium = createIsland shapedAs radial(factor = 1.47)
   //export(medium)
 
+  info("Configuring large island")
   val large = createIsland shapedAs radial(factor = 1.07) withSize 2048 having 4096.faces
   //export(large)
 
-  val always = createIsland usingSeed "64236166-165d-47f0-a4fd-ed2c443ff834" withSize 800 having 1024.faces
+  info("Configuring a reproducible island")
+  val always = createIsland usingSeed "64236166-165d-47f0-a4fd-ed2c443ff834"
   export(always)
 
 
--- src/main/scala/eu/ace_design/island/map/IslandBuilder.scala
+++ src/main/scala/eu/ace_design/island/map/IslandBuilder.scala
@@ -1,7 +1,8 @@
 package eu.ace_design.island.map
 
 import eu.ace_design.island.geom._
-import eu.ace_design.island.map.processes.Process
+import eu.ace_design.island.map.processes.{RandomizedProcess, Process}
+import scala.util.Random
 
 /**
  * An IslandBuilder is a sequence of Process used to build an Island map
@@ -21,10 +22,23 @@ trait IslandBuilder {
    * @param m the mesh to be used as entry point
    * @return an island map, based on the sequential execution of the different steps of this builder
    */
-  def apply(m: Mesh): IslandMap = {
+  def apply(m: Mesh, random: Random = new Random()): IslandMap = {
     require(m.size.isDefined, "Cannot build an island without knowing its size")
     require(m.size.get == size, "Size of mesh and map must be consistent")
-    (IslandMap(m) /: steps) { (map, p) => p(map) }
+    (IslandMap(m) /: preProcess(random, steps)) { (map, p) => p(map) }
+  }
+
+  /**
+   * Propagate a shared random generator to each process when needed (i.e., a process p is a Randomized one)
+   * @param random the random generator to be used
+   * @param steps the processes to pre-process.
+   * @return a sequence of function to be used to transform a given map into an updated one.
+   */
+  def preProcess(random: Random, steps: Seq[Process]): Seq[IslandMap => IslandMap] = {
+    steps map { step => step  match {
+      case randomized: RandomizedProcess => randomized(random) _
+      case deterministic: Process        => deterministic.apply _
+    } }
   }
 
 }
