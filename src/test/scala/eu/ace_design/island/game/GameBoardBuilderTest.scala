5ec074abff99fc2410f0622930b14729464b7c60
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-01-02 10:59:59 +0100

    GameBoardBuilder test updated (bug fixed in the map description)

--- src/test/scala/eu/ace_design/island/game/GameBoardBuilderTest.scala
+++ src/test/scala/eu/ace_design/island/game/GameBoardBuilderTest.scala
@@ -11,16 +11,23 @@ class GameBoardBuilderTest extends SpecificationWithJUnit {
 
   "GameBoardBuilderTest Specifications".title
 
+  import GameBoardBuilderDataSet.island
+
   "The GameBoardBuilder" should {
 
     val m = IslandMap(MeshBuilderTestDataSet.mesh) // m.size == 200
-    val builder = new GameBoardBuilder()   // chunk = DEFAULT_TILE_UNIT = 10
+    val builder = new GameBoardBuilder() // chunk = DEFAULT_TILE_UNIT = 10
     val triangle = Set(Point(5.0, 6.0), Point(18.9, 28.3), Point(26.4, 15.5))
-    val board = builder(GameBoardBuilderDataSet.island)
+
+    val board = builder(island)
+    val f0 = island.convexHull(island.face(0)).toSet
+    val f1 = island.convexHull(island.face(1)).toSet
+    val f2 = island.convexHull(island.face(2)).toSet
+    val f3 = island.convexHull(island.face(3)).toSet
 
     "reject a chunk size incompatible with the size of the map" in {
       val erroneous = new GameBoardBuilder(chunk = 11)
-      erroneous(m) must throwAn[IllegalArgumentException]  // 200 % 11 <> 0
+      erroneous(m) must throwAn[IllegalArgumentException] // 200 % 11 <> 0
     }
 
     "ensure that the size of the game board is the size of the used map" in {
@@ -29,20 +36,33 @@ class GameBoardBuilderTest extends SpecificationWithJUnit {
     }
 
     "identify in which tile is located a given point" in {
-      builder.locate(Point(0.0,0.0))     must_== (0,0)
-      builder.locate(Point(99.0, 188.0)) must_== (9, 18)
+      builder.locate(Point(0.0, 0.0)) must_==(0, 0)
+      builder.locate(Point(99.0, 188.0)) must_==(9, 18)
     }
 
     "identify the bounding box of a given face" in {
       // triangle(0) \in (0,0), triangle(1) \in (1,2) and triangle(2) \in (2,1)
       builder.boundingBox(triangle) must_== GameBoardBuilderDataSet.tiles
+      builder.boundingBox(f0) must_== Set((0, 0), (1, 0), (2, 0), (3, 0),
+        (0, 1), (1, 1), (2, 1), (3, 1))
+      builder.boundingBox(f1) must_== Set((0, 0), (1, 0), (2, 0),
+        (0, 1), (1, 1), (2, 1),
+        (0, 2), (1, 2), (2, 2),
+        (0, 3), (1, 3), (2, 3))
+      builder.boundingBox(f2) must_== Set((0, 1), (1, 1), (2, 1), (3, 1),
+        (0, 2), (1, 2), (2, 2), (3, 2),
+        (0, 3), (1, 3), (2, 3), (3, 3))
+      builder.boundingBox(f3) must_== Set((2, 0), (2, 1), (2, 2), (2, 3),
+        (3, 0), (3, 1), (3, 2), (3, 3))
     }
 
     "identify the tiles covered by a given face" in {
       val coverage = builder.coverage(triangle)
       coverage.values.sum must beCloseTo(100.0, 0.0001) // The coverage algorithm is "almost" exact
       // The triangle is not located on the upper right (0,2) and lower left (2,0) of its bounding box
-      coverage.keys.toSet must_== Set((0,0), (0,1), (1,0), (1,1), (1,2), (2,1), (2,2))
+      coverage.keys.toSet must_== Set((0, 0), (0, 1), (1, 0), (1, 1), (1, 2), (2, 1), (2, 2))
+
+
     }
 
     "build a game board of size 3 x 3 using the example island" in {
@@ -54,53 +74,68 @@ class GameBoardBuilderTest extends SpecificationWithJUnit {
       import eu.ace_design.island.map.resources.ExistingResources._
       import eu.ace_design.island.map.resources.Soils._
       import eu.ace_design.island.map.resources.Conditions._
+      import GameBoardBuilderDataSet.island
 
-      val f0 = Set(Point(0.0,0.0), Point(30.0, 0.0), Point(20.0, 10.0))
+
       val p0 = builder.production(f0, WOOD, NORMAL, FAIR, 150.0).toMap
-      p0.keys must_== Set((0,0), (1,0), (2,0))
-      (p0.values map { _.resource }).toSet must_== Set(WOOD)
+      p0.keys must_== Set((0, 0), (1, 0), (2, 0))
+      (p0.values map {
+        _.resource
+      }).toSet must_== Set(WOOD)
 
-      val f1 = Set(Point(0.0,0.0), Point(0.0, 30.0), Point(20.0, 10.0))
+      val f1 = island.convexHull(island.face(1)).toSet
       val p1 = builder.production(f1, ORE, POOR, FAIR, 300.0).toMap
-      p1.keys must_== Set((0,0), (0,1), (0,2), (1,0), (1,1))
-      (p1.values map { _.resource }).toSet must_== Set(ORE)
+      p1.keys must_== Set((0, 0), (0, 1), (0, 2), (1, 0), (1, 1))
+      (p1.values map {
+        _.resource
+      }).toSet must_== Set(ORE)
 
-      val f2 = Set(Point(30.0,30.0), Point(0.0, 30.0), Point(20.0, 10.0))
+      val f2 = island.convexHull(island.face(2)).toSet
       val p2 = builder.production(f2, FLOWER, FERTILE, HARSH, 300.0).toMap
-      p2.keys must_== Set((0,2), (1,2), (2,2), (1,1), (2,1))
-      (p2.values map { _.resource }).toSet must_== Set(FLOWER)
+      p2.keys must_== Set((0, 2), (1, 2), (2, 2), (1, 1), (2, 1))
+      (p2.values map {
+        _.resource
+      }).toSet must_== Set(FLOWER)
 
-      val f3 = Set(Point(30.0,30.0), Point(30.0, 0.0), Point(20.0, 10.0))
+      val f3 = island.convexHull(island.face(3)).toSet
       val p3 = builder.production(f3, None, POOR, EASY, 300.0).toMap
       p3.keys must_== Set()
-      (p3.values map { _.resource }).toSet must_== Set()
+      (p3.values map {
+        _.resource
+      }).toSet must_== Set()
 
     }
 
     "Assign relevant resources to each tile" in {
       import eu.ace_design.island.map.resources.ExistingResources._
-      def oracle(x: Int, y: Int): Set[Set[Resource]] = (x,y) match {
-        case (0,0) => Set(Set(WOOD, ORE))
-        case (1,0) => Set(Set(WOOD, ORE))
-        case (2,0) => Set(Set(WOOD, FLOWER), Set(WOOD))
-        case (0,1) => Set(Set(ORE))
-        case (1,1) => Set(Set(ORE, WOOD), Set(ORE, FLOWER))
-        case (2,1) => Set(Set(WOOD, FLOWER), Set(FLOWER))
-        case (0,2) => Set(Set(ORE, WOOD), Set(ORE, FLOWER))
-        case (1,2) => Set(Set(WOOD), Set(FLOWER))
-        case (2,2) => Set(Set(WOOD, FLOWER), Set(FLOWER))
+      def oracle(x: Int, y: Int): Set[Set[Resource]] = (x, y) match {
+        case (0, 0) => Set(Set(WOOD, ORE))
+        case (1, 0) => Set(Set(WOOD, ORE))
+        case (2, 0) => Set(Set(WOOD, FLOWER), Set(WOOD))
+        case (0, 1) => Set(Set(ORE))
+        case (1, 1) => Set(Set(ORE, WOOD), Set(ORE, FLOWER))
+        case (2, 1) => Set(Set(WOOD, FLOWER), Set(FLOWER), Set(WOOD))
+        case (0, 2) => Set(Set(ORE, WOOD), Set(ORE, FLOWER))
+        case (1, 2) => Set(Set(WOOD), Set(FLOWER))
+        case (2, 2) => Set(Set(WOOD, FLOWER), Set(FLOWER), Set(WOOD))
         case _ => throw new IllegalArgumentException()
       }
-      board.tiles.keys foreach { case (x,y) =>
-        val expected = oracle(x,y)
-        val actual = board.produces(x,y)
-        println(s"($x,$y) / expected: $expected / actual: $actual")
+      def assess(x: Int, y: Int) = {
+        val expected = oracle(x, y)
+        val actual = board.produces(x, y)
+        // println(s"($x,$y) / expected: $expected / actual: $actual")
         expected.intersect(Set(actual)) must not(beEmpty)
       }
-      true must beTrue
+      assess(0, 0)
+      assess(0, 1)
+      assess(0, 2)
+      assess(1, 0)
+      assess(1, 1)
+      assess(1, 2)
+      assess(2, 0)
+      assess(2, 1)
+      assess(2, 2)
     }
-
-
   }
 }
 
@@ -147,7 +182,7 @@ object GameBoardBuilderDataSet {
   val eReg = EdgeRegistry() + Edge(0,1) + Edge(1,2) + Edge(2,0) + Edge(2,3) +
                               Edge(3,0) + Edge(3,4) + Edge(4,2) + Edge(4,1)
   // We do not exploit neighborhood relationship between faces to build the board => set to None (default value)
-  val fReg = FaceRegistry() + Face(5,Seq(0,1,2)) + Face(6, Seq(0,2,3)) + Face(7, Seq(3,2,4)) + Face(8, Seq(4,2,1))
+  val fReg = FaceRegistry() + Face(5,Seq(0,1,2)) + Face(6, Seq(2,3,4)) + Face(7, Seq(3,5,6)) + Face(8, Seq(1,6,7))
 
   val properties = PropertySet() +
                     // Face 0: TEMPERATE_DECIDUOUS_FOREST, NORMAL, FAIR
