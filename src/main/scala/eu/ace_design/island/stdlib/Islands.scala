3a80af091e724cc9f4f96368fb686d433a116d5a
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-01-04 11:40:39 +0100

    create a standard library for islands

---
+++ src/main/scala/eu/ace_design/island/stdlib/Islands.scala
@@ -0,0 +1,59 @@
+package eu.ace_design.island.stdlib
+
+import eu.ace_design.island.dsl.DiSLand
+import eu.ace_design.island.map.IslandMap
+import eu.ace_design.island.map.processes.AssignPitch
+
+/**
+ * This file is a library of pre-existing islands.
+ **/
+object Islands extends DiSLand {
+
+  /**
+   * A sample with inner lakes, small and quick to generate
+   */
+  lazy val default: IslandMap = {
+    createIsland shapedAs radial(factor = 1.09) usingSeed 0x24F84E32B99D3CF5L
+  }
+
+  /**
+   * Shaped as a tore, with a large inner lake in the middle of the island (e.g., a crater lake)
+   */
+  lazy val donuts: IslandMap = {
+    createIsland shapedAs donut(80.percent, 20.percent) withSize 400 having 1200.faces builtWith Seq(
+      plateau(23), flowing(rivers = 15, distance = 0.2), withMoisture(soils.normal, distance = 400),
+      AssignPitch, usingBiomes()
+    )
+  }
+
+  /**
+   * An island shape as an ellipsis, inspired by the Ile de la Tortue in Haiti
+   * Ref: http://en.wikipedia.org/wiki/Tortuga_Island,_Haiti
+   */
+  lazy val tortuga: IslandMap =  {
+    createIsland shapedAs ellipsis(x=95.percent, y=40.percent, theta=20) builtWith Seq(
+      plateau(45), flowing(rivers = 30, distance = 0.6), withMoisture(soils.wet, distance = 100),
+      AssignPitch, usingBiomes(WhittakerDiagrams.caribbean)
+    ) usingSeed 0x24F84E32B98D3CF5L withSize 1600 having 3000.faces
+  }
+
+  /**
+   * This island is inspired by the Realm of Oz, a squared country surrounded by an impassable desert (here the ocean).
+   */
+  lazy val ozRealm: IslandMap = {
+    createIsland shapedAs oz() builtWith Seq(
+      plateau(100), flowing(rivers = 45, distance = 0.3), withMoisture(soils.normal, distance = 400),
+      AssignPitch, usingBiomes() ) usingSeed 0x5DA96217A1606058L
+  }
+
+  /**
+   * A nordic island (imaginary), shaped as a butterfly
+   */
+  lazy val sommerfugloya: IslandMap = {
+    createIsland shapedAs radial(factor = 1.26) builtWith Seq(
+      flatDistribution(100), flowing(rivers = 30, distance = 0.4), withMoisture(soils.wet, distance = 200),
+      AssignPitch, usingBiomes(WhittakerDiagrams.nordic)
+    ) usingSeed 0x9AC771d247f74037L withSize 2000 having 4096.faces
+  }
+
+}
--- src/main/scala/eu/ace_design/island/stdlib/WhittakerDiagrams.scala
+++ src/main/scala/eu/ace_design/island/stdlib/WhittakerDiagrams.scala
@@ -7,8 +7,9 @@ import eu.ace_design.island.map.resources.{WhittakerParser, WhittakerDiagram}
  */
 object WhittakerDiagrams {
 
-  lazy val complete: WhittakerDiagram = WhittakerParser(completeTemplate)
-
+  lazy val caribbean: WhittakerDiagram = WhittakerParser(caribbeanTemplate)
+  lazy val nordic: WhittakerDiagram    = WhittakerParser(nordicTemplate)
+  lazy val complete: WhittakerDiagram  = WhittakerParser(completeTemplate)
 
   /**
    * Templates available to represents different ecosystems
@@ -17,22 +18,45 @@ object WhittakerDiagrams {
    */
 
   private val completeTemplate =
+  """
+    |@ice_level 1300
+    |#    0%  10% 20% 30% 40% 50% %60 70% 80% 90%
+    |-    ALP ALP ALP ALP ALP SNO SNO SNO SNO SNO
+    |2100 ALP ALP TUN TUN TUN TUN TAI TAI SNO SNO
+    |1900 ALP TUN TUN TUN TUN TUN TAI TAI TAI SNO
+    |1700 teD TUN TUN SHR SHR SHR TAI TAI TAI TAI
+    |1500 teD teD teD SHR SHR SHR teF TAI TAI TAI
+    |1300 teD teD SHR SHR SHR SHR teF teF TAI TAI
+    |1100 teD teD GRA GRA teF teF teF teF teF teF
+    | 900 teD teD GRA GRA GRA GRA teF teF teF teR
+    | 700 teD GRA GRA GRA teF teF teF teF teR teR
+    | 500 teD GRA GRA GRA trS trS teR teR teR teR
+    | 300 STD GRA GRA GRA trS trS teF trF trF trF
+    | 100 STD STD GRA trS trS trS trS trF trF trF
+    |  50 STD STD STD trS trS trF trF trF trF MAN
+    |  25 BEA BEA BEA BEA BEA trF trF trF MAN MAN
+  """.stripMargin
+
+  private val caribbeanTemplate =
+  """
+    |@ice_level 1600
+    |#    0%  10% 20% 30% 40% 50% %60 70% 80% 90%
+    |-    SHR SHR trS trS trS trS trS trS trF trF
+    |150  SHR SHR SHR SHR trS trS trS trF trF trF
+    |50   SHR SHR SHR trS trS trF trF trF trF trF
+    |20   SHR SHR BEA BEA BEA BEA MAN MAN MAN MAN
+  """.stripMargin
+
+  private val nordicTemplate =
     """
-      |@ice_level 1300
+      |@ice_level 900
       |#    0%  10% 20% 30% 40% 50% %60 70% 80% 90%
       |-    ALP ALP ALP ALP ALP SNO SNO SNO SNO SNO
-      |2100 ALP ALP TUN TUN TUN TUN TAI TAI SNO SNO
-      |1900 ALP TUN TUN TUN TUN TUN TAI TAI TAI SNO
-      |1700 teD TUN TUN SHR SHR SHR TAI TAI TAI TAI
-      |1500 teD teD teD SHR SHR SHR teF TAI TAI TAI
-      |1300 teD teD SHR SHR SHR SHR teF teF TAI TAI
-      |1100 teD teD GRA GRA teF teF teF teF teF teF
-      | 900 teD teD GRA GRA GRA GRA teF teF teF teR
-      | 700 teD GRA GRA GRA teF teF teF teF teR teR
-      | 500 teD GRA GRA GRA trS trS teR teR teR teR
-      | 300 STD GRA GRA GRA trS trS teF trF trF trF
-      | 100 STD STD GRA trS trS trS trS trF trF trF
-      |  50 STD STD STD trS trS trF trF trF trF MAN
-      |  25 BEA BEA BEA BEA BEA trF trF trF MAN MAN
+      |800  ALP ALP TUN TUN TUN TUN TAI TAI TAI TAI
+      |400  TUN TUN TUN TUN teF teF teF teF TAI TAI
+      |150  GRA TUN TUN teF teF teF teF teF teF TAI
+      |50   GRA GRA teF teF teF teF teF teF teF teF
+      |15   GRA GRA BEA BEA BEA BEA BEA teF teF teF
     """.stripMargin
+
 }
