a42f44d86f62842af3f5cb9019aa752ea689b2f9
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-02-12 19:02:44 +0100

    introducing 3 sub-modules

---
+++ bot/src/main/java/eu/ace_design/island/bot/MyBot.java
@@ -0,0 +1,20 @@
+package eu.ace_design.island.bot;
+
+
+public class MyBot implements IExplorerRaid {
+
+	@Override
+	public void initialize(String context) {
+		throw new UnsupportedOperationException("IExplorerRaid::initialize is not yet implemented");
+	}
+
+	@Override
+	public String takeDecision() {
+		throw new UnsupportedOperationException("IExplorerRaid::takeDecision is not yet implemented");
+	}
+
+	@Override
+	public void acknowledgeResults(String results) {
+		throw new UnsupportedOperationException("IExplorerRaid::acknowledgeResults is not yet implemented");
+	}
+}
