a578adf99de5fa30a541969e041911451a1dfbba
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-02-25 17:26:54 +0100

    Timeout supported in the util lib

---
+++ engine/src/main/scala/eu/ace_design/island/util/Timeout.scala
@@ -0,0 +1,9 @@
+package eu.ace_design.island.util
+
+/**
+ * This file is part of the island project
+ * @author mosser (25/02/2015, 16:53)
+ **/
+trait Timeout {
+
+}
---
+++ engine/src/test/scala/eu/ace_design/island/util/TimeoutTest.scala
@@ -0,0 +1,12 @@
+package eu.ace_design.island.util
+
+import org.specs2.mutable._
+import org.junit.runner.RunWith
+import org.specs2.runner.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class TimeoutTest extends SpecificationWithJUnit {
+
+  "Timeout Specifications".title
+
+}
