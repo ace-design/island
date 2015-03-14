27b2eeb77fa02028d1c6bf9fb38d7172f7d5b012
Sebastien Mosser <sebastien.mosser@gmail.com>
2015-03-14 17:36:24 +0000

    cleaning Action.scala

--- engine/src/main/scala/eu/ace_design/island/game/Actions.scala
+++ engine/src/main/scala/eu/ace_design/island/game/Actions.scala
@@ -87,25 +87,6 @@ case class Stop() extends Action {
  */
 case class Land(creek: String, people: Int) extends Action          {
 
-  /*override protected def build(board: GameBoard, game: Game, overhead: Int): Result = {
-    debug("  Land:")
-    if(people >= game.crew.complete)
-      throw new IllegalArgumentException("At least one men must stay on board")
-    val creeks = board.findPOIsByType(Creek(null, null))
-    val (cost, loc) = creeks find  { case (loc,c) => c.identifier == creek } match {
-      case None => throw new IllegalArgumentException(s"Unknown creek identifier [$creek]")
-      case Some((loc,poi)) => {
-        val origin = game.boat.getOrElse((board.size / 2, board.size / 2))
-        val distance = Math.sqrt(Math.pow(loc._1- origin._1,2) + Math.pow(loc._2- origin._2,2))
-        val ratio = board.m.size.toFloat / board.size / 10
-        debug(s"    distance: $distance / ratio: $ratio ")
-        ((overhead + distance * ratio) * variation, loc)
-      }
-    }
-    info(s"  Land: cost = $cost")
-    MovedBoatResult(cost.ceil.toInt, loc, people)
-  } */
-
   override def computeCost(board: GameBoard, game: Game): Double = {
     val alreadyLanded = game.crew.landed
     val creekLocation = board.findPOIsByType(Creek(null, null)).find { case (loc,c) => c.identifier == creek }.get._1
@@ -128,37 +109,6 @@ case class Land(creek: String, people: Int) extends Action          {
  * @param direction
  */
 case class MoveTo(override val direction: Directions.Direction) extends ActionWithDirection {
-
-  import eu.ace_design.island.map.resources.PIXEL_FACTOR
-
-  /*override protected def build(board: GameBoard, game: Game, overhead: Int): Result = {
-    debug("  MoveTo:")
-    if(game.boat.isEmpty)
-      throw new IllegalArgumentException("Cannot move without having landed before")
-    val loc = game.crew.location.get  // cannot be None as game.boat is not empty
-    val oldTile = board.at(loc._1, loc._2)
-    val updatedLoc = Directions.move(loc._1, loc._2, direction)
-    if(! board.tiles.keySet.contains(updatedLoc))
-      throw new IllegalArgumentException("Congrats, you just fall out of the world limit.")
-
-    val newTile = board.at(updatedLoc._1, updatedLoc._2)
-    // Now we can move. Move is a function of men, altitude difference and biomes to cross.
-    val cost = {
-      val men = game.crew.landed
-      val rise = Math.abs(oldTile.altitude - newTile.altitude) // altitude already stored as meters
-      val run = board.tileUnit.toDouble * PIXEL_FACTOR // run to be transformed from pixel to meters
-      val pitch = 100 * (rise / run) // http://en.wikipedia.org/wiki/Grade_(slope)
-      val pitchFactor = Math.min(0.5, Math.abs(100 - pitch))
-      val biomes = newTile.biomes
-      val crossFactor = (0.0 /: biomes) { (acc, value) => acc + (value._1.crossFactor * value._2)} / 100
-      val factor = (pitchFactor + 2 * crossFactor) / 3
-      debug(s"    men: $men / pitch: $pitchFactor / cross: $crossFactor")
-      debug(s"     ==>> factor: $factor")
-      (overhead + (2.0 * men * factor)) * variation
-    }
-    info(s"  MoveTo: cost = $cost")
-    MovedCrewResult(cost = cost.ceil.toInt, loc = updatedLoc)
-  } */
 
   override def computeCost(board: GameBoard, game: Game): Double = {
     val movingRawFactor = 1 + Game.movingCostModel(game.normalizeMen) // [0,1] => [0,1]
@@ -183,26 +133,6 @@ case class MoveTo(override val direction: Directions.Direction) extends ActionWi
  */
 case class Scout(direction: Directions.Direction) extends ActionWithDirection {
 
-  /*override protected def build(board: GameBoard, game: Game, overhead: Int): Result = {
-    if(game.boat.isEmpty)
-      throw new IllegalArgumentException("Cannot move without having landed before")
-    val loc = game.crew.location.get  // cannot be None as game.boat is not empty
-    val oldTile = board.at(loc._1, loc._2)
-    val updatedLoc = Directions.move(loc._1, loc._2, direction)
-    if(! board.tiles.keySet.contains(updatedLoc))
-      return ScoutResult(cost = overhead, resources = Set(), altitude = 0, unreachable = true)
-    val newTile = board.at(updatedLoc._1, updatedLoc._2)
-
-    val res = newTile.stock map { _.resource.asInstanceOf[Resource] }
-    val delta = oldTile.altitude - newTile.altitude
-    val cost = {
-      val men = game.crew.landed
-      val factor = (0.0 /: newTile.biomes) { (acc, value) => acc + (value._1.crossFactor * value._2)} / 100
-      (overhead + (men * factor)) * variation
-    }
-    ScoutResult(cost = cost.ceil.toInt, resources = res, altitude = delta.ceil.toInt)
-  } */
-
   override def computeCost(board: GameBoard, game: Game): Double = {
     val next = nextLoc(game)
     val factor = board.tiles.get(next) match {
@@ -211,7 +141,6 @@ case class Scout(direction: Directions.Direction) extends ActionWithDirection {
     }
     (1 + Random.nextInt(3)) * factor
   }
-
 
   override def buildResult(board: GameBoard, game: Game): Result = {
     require(game.crew.location.isDefined, "Cannot scout without having landed before")
@@ -231,34 +160,6 @@ case class Scout(direction: Directions.Direction) extends ActionWithDirection {
 
 // { "action": "explore" }
 case class Explore() extends Action {
-
-  /*override protected def build(board: GameBoard, game: Game, overhead: Int): Result = {
-    if(game.boat.isEmpty)
-      throw new IllegalArgumentException("Cannot move without having landed before")
-    val loc = game.crew.location.get  // cannot be None as game.boat is not empty
-    val tile = board.at(loc._1, loc._2)
-    val resources = tile.stock map { stock =>
-      val max = stock.resource.perHectare.toDouble / (board.tileUnit * board.tileUnit * 100)
-      val alreadyExtracted = game.harvested(stock.resource,loc)
-      val avail = stock.amount - alreadyExtracted
-      val amount = avail match {
-        case a if a <= max / 3      => ResourceLevels.LOW
-        case a if a >= 2 * max / 3  => ResourceLevels.HIGH
-        case _ => ResourceLevels.MEDIUM
-      }
-      val condition = stock.extraction match {
-        case e if e <= 0.6 => Conditions.HARSH
-        case e if e >= 1.3 => Conditions.EASY
-        case _ => Conditions.FAIR
-      }
-      ResourceExploration(resource = stock.resource, amount = amount, condition = condition)
-    }
-    val pois = board.pois.getOrElse((loc._1, loc._2), Seq()).toSet
-    val factor = (0.0 /: tile.biomes) { (acc, value) => acc + (value._1.crossFactor * value._2)} / 100
-    val men = game.crew.landed
-    val cost =  (overhead + (3 * men * factor)) * variation
-    ExploreResult(cost = cost.ceil.toInt, resources = resources, pois = pois)
-  }*/
 
   override def computeCost(board: GameBoard, game: Game): Double = {
     val biomeFactor = board.biomeFactor(game.crew.location.get)
@@ -282,33 +183,10 @@ case class Explore() extends Action {
 // { "action": "exploit", "parameters": { "resource": "..." } }
 case class Exploit(resource: PrimaryResource) extends Action {
 
-  /*override protected def build(board: GameBoard, game: Game, overhead: Int): Result = {
-    if(game.boat.isEmpty)
-      throw new IllegalArgumentException("Cannot move without having landed before")
-    val loc = game.crew.location.get  // cannot be None as game.boat is not empty
-    val tile = board.at(loc._1, loc._2)
-    val extracted = tile.stock.find(s => s.resource == resource) match {
-      case None => throw new IllegalArgumentException(s"No resource [$resource] available on the current tile")
-      case Some(stock) => {
-        val alreadyExtracted = game.harvested(resource,loc)
-        val avail = stock.amount - alreadyExtracted
-        // do the extraction
-        val theoretical = (game.crew.landed * 0.5 * stock.extraction * avail * variation).ceil.toInt
-        val amount = Math.min(avail, theoretical)
-        amount
-      }
-    }
-    val boat = game.boat.get
-    val distance = Math.sqrt(Math.pow(loc._1 -boat._1,2)+Math.pow(loc._2 -boat._2,2))
-    val cost = overhead + ( 2 * distance * game.crew.landed * extracted / 10.0)
-    ExploitResult(cost = cost.ceil.toInt, amount = extracted, r = resource)
-  } */
-
   override def computeCost(board: GameBoard, game: Game): Double = {
     val exploitationFactor = Game.exploitationCostModel(game.normalizeMen)
     (game.crew.landed * exploitationFactor * resource.difficulty) + Math.sqrt(game.distanceToBoat)
   }
-
 
   override def buildResult(board: GameBoard, game: Game): Result = {
     require(game.boat.isDefined, "Cannot explore without having landed before")
