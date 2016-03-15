1c5a5bef70dbdab649872a83a132ff0f14bef5e8
Flavian JACQUOT <flavian.jacquot@gmail.com>
2016-03-15 11:35:36 +0100

    Solved issue #64 : restore previous System.out for logging; Added the corresponding test

--- runner/src/test/java/RunnerTest.java
+++ runner/src/test/java/RunnerTest.java
@@ -2,6 +2,7 @@ import eu.ace_design.island.runner.Runner;
 import eu.ace_design.island.runner.sample.MyBot;
 import org.junit.Test;
 import java.io.File;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -22,6 +23,21 @@ public class RunnerTest {
 		assertTrue(d2 > d1);
 	}
 
+	@Test
+	public void testSystemOutRestored() throws Exception {
+		Path out = Files.createTempDirectory("test-sysout-restore");
+		File outputTxt = new File(out.toAbsolutePath().toString() + "/log.txt");
+		PrintStream old = System.out;
+		PrintStream ps = new PrintStream(outputTxt);
+		System.out.println("Setting System.out to file "+outputTxt);
+		System.setOut(ps);
+		run(out);
+
+		assertTrue(outputTxt.exists());
+
+		assertEquals(ps,System.out);
+		System.setOut(old);
+	}
 	private static void run(Path out) throws Exception {
 		Runner.run(MyBot.class)
 				.exploring(load("map.json"))   // A File containing a map as a JSON object
